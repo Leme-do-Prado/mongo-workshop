67d0aa07de21b37bc6a4770a2818a17e74c7dda2
leme-do-prado <urutuestrela@gmail.com>
2023-04-13 12:50:49 -0300

    Criados PostResources, PostService

---
+++ src/main/java/com/lemedoprado/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lemedoprado.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lemedoprado.workshopmongo.domain.Post;
+import com.lemedoprado.workshopmongo.repositories.PostRepository;
+import com.lemedoprado.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if(user==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
