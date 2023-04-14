07a1c9930377b883a11b9c9058c4cc712181557c
leme-do-prado <urutuestrela@gmail.com>
2023-04-14 11:43:57 -0300

    CommentDTO e relações implementados! xD

--- src/main/java/com/lemedoprado/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lemedoprado/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lemedoprado.workshopmongo.domain.Post;
 import com.lemedoprado.workshopmongo.domain.User;
 import com.lemedoprado.workshopmongo.dto.AuthorDTO;
+import com.lemedoprado.workshopmongo.dto.CommentDTO;
 import com.lemedoprado.workshopmongo.repositories.PostRepository;
 import com.lemedoprado.workshopmongo.repositories.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 
 		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.save(Arrays.asList(post1, post2));
 		
