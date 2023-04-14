c5eb366f5ede09ca4786a13a4e250787b863d92c
leme-do-prado <urutuestrela@gmail.com>
2023-04-14 12:21:47 -0300

    Consulta com @Query

--- src/main/java/com/lemedoprado/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/lemedoprado/workshopmongo/repositories/PostRepository.java
@@ -8,6 +8,9 @@ import com.lemedoprado.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, /pattern, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
