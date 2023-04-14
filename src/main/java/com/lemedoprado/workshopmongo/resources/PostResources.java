2d6ae36c1364f08cba2356bd2536c886ae3b981d
leme-do-prado <urutuestrela@gmail.com>
2023-04-14 12:09:45 -0300

    Consultas por título implementadas! xD

--- src/main/java/com/lemedoprado/workshopmongo/resources/PostResources.java
+++ src/main/java/com/lemedoprado/workshopmongo/resources/PostResources.java
@@ -1,14 +1,17 @@
 package com.lemedoprado.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lemedoprado.workshopmongo.domain.Post;
-import com.lemedoprado.workshopmongo.dto.UserDTO;
+import com.lemedoprado.workshopmongo.resources.util.URL;
 import com.lemedoprado.workshopmongo.services.PostService;
 
 @RestController
@@ -23,5 +26,12 @@ public class PostResources {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+	text = URL.decodeParam(text);
+	List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+}
 
 }
